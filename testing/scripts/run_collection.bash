a834225c449112f06bf6349d7faad72de1d34c50
Marina Karpovsky <karpovskaamarina@gmail.com>
2024-02-07 18:44:09 +0000

    create docker image newman-html

--- testing/scripts/run_collection.bash
+++ testing/scripts/run_collection.bash
@@ -13,11 +13,15 @@ docker run \
     -v "$PWD":/usr/src/app \
     -w /usr/src/app \
     -t \
-    postman/newman run \
+    newman-html run \
         $1 \
         -e $2 \
         -d $3 \
-        --delay-request 1000
+        --delay-request 1000 \
+        -r cli,htmlextra \
+        --reporter-htmlextra-export="./reports/report.html"
         #  \
         # -r cli,json \
         # --reporter-json-export="./results.json"
+        #--reporters cli,htmlextra - reporter-htmlextra-export "./reports/report.html" 
+        
