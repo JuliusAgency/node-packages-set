e3d77a8f31a675e34c2348f661bac8f3cab99027
Marina Karpovsky <karpovskaamarina@gmail.com>
2024-04-28 13:53:27 +0000

    update collections

--- testing/scripts/run_test.bash
+++ testing/scripts/run_test.bash
@@ -2,11 +2,14 @@
 echo "=================================================="
 echo "test will run from the folder $PWD"
 echo with:
-collection="./test-sanity.postman_collection.json"
+#collection="./test-sanity.postman_collection.json"
+collection="./test-auth.postman_collection.json"
 echo "  $collection"
 environment="./$1.postman_environment.json"
 echo "  $environment"
-data="./test-data.csv"
+#data="./test-data.csv"
+#data="./test-data-auth-acl.csv"
+data="./test-data-auth-rbac.csv"
 echo "  $data"
 echo "=================================================="
 source ./scripts/run_collection.bash $1 $collection $environment $data
